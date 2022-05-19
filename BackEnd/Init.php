5560e670be970e15c865b034565417e35145a72a
Leonel Costa <53113015+leonelbahia@users.noreply.github.com>
2022-05-18 22:28:57 -0300

    Add files via upload

--- BackEnd/Init.php
+++ BackEnd/Init.php
@@ -1,8 +1,13 @@
 <?php namespace Init;
+
+$sectionRouter = $_GET['sectionRouter'];
+
 class init {
-    public static function in($path='/', $backend=index, $frontend=index){
-        if (file_exists("FrontEnd/Html/{$frontend}.html")) {require("FrontEnd/Html/{$frontend}.html");}
-        else {echo "Error: Define the tag fragment in Html.";}       
+    public static function in($backend=index, $frontend=['index']){
+        foreach ($frontend as $file) {
+            if (file_exists("FrontEnd/Html/{$file}.html")) {require("FrontEnd/Html/{$file}.html");}
+            else {echo "Error: Define the tag fragment in Html.";}
+        }
     }
 };
 ?>
